51065137079bd82147b35a8810f4b4dbd979f44a
Olamidee007 <lamide.dammy@gmail.com>
2022-05-24 01:36:53 +0100

    commit to git

--- buy_now.js
+++ buy_now.js
@@ -1,11 +1,29 @@
 // Adding a variable to store data
-const buyIt = document.querySelector(.'buy-submit')
+const buyIt = document.querySelector('.buy-submit')
+const buYer = document.querySelector('#guessField')
+const buyerInput=document.querySelector('.field-input')
 
 // Arrow function
 const buyNow = (some_more) =>{
   buyIt.textContent = some_more;
+  buYer.textContent = add_more;
+  buyerInput.textContent = get_more
+
   if (some_more === '') {
     buyIt.textContent = 'Please type in letters';
-    return;
   }
+
+  if (add_more === 'YOUR NAME') {
+    buYer.textContent = 'Please Enter Custom letters';
+  }
+  else if (get_more === <=15) {
+    buyerInput.textContent = 'Congratulations, You have a customized plaque';
+  }
+  if (get_more === >15) {
+    buyerInput.textContent = 'Sorry, you have exceeded the number of available letters'
+  }
+}
+
+buyIt.onclick = (o)=>{
+  buyNow(o.target.value)
 }
